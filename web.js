abdd2b319209d495daef85833790cb667b5ba148
Andrei Stroescu <andreiweb@gmail.com>
2013-07-09 17:32:18 +0000

    a

---
+++ web.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var outData;
+fs.readFile('index.html', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  outData=data;
+});
+
+app.get('/', function(request, response) {
+
+    response.send(outData);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
